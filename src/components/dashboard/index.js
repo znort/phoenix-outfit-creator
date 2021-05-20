62f5ef598dc229646c4e1cfd50d806de4d0340f7
StefanoMnS <Stefano.Lissia@mnscorp.net>
2021-05-20 14:00:47 +0100

    initial commit

---
+++ src/components/dashboard/index.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import '../../styles/dashboard.scss';
+
+
+class Dashboard extends Component {
+
+  render() {
+    return (
+      
+        <h1>Dashboard page</h1>
+        
+    
+    )
+  }
+}
+
+
+export default Dashboard
---
+++ src/components/products/index.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import '../../styles/products.scss';
+
+
+class Products extends Component {
+
+  render() {
+    return (
+      
+        <h1>Products page</h1>
+        
+    
+    )
+  }
+}
+
+
+export default Products
