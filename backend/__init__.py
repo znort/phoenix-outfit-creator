aaaaee3e9dcc348aa493a698ac292e7e137962c3
Abhilash Nair <abhilash.b.nair@marks-and-spenser.com>
2021-05-20 17:06:23 +0100

    User outfits first working commit

--- backend/__init__.py
+++ backend/__init__.py
@@ -28,22 +28,51 @@ class OutfitCreatorService(AbstractApplicationService):
         return web.Response(body="hello", status=200)
 
     async def post_user(self, request):
-        user_request = await request.json()
-        self.user_datastore.add(User(**user_request))
-        return web.Response(body="User Created", status=201)
+        try:
+            user_request = await request.json()
+            user = User(**user_request)
+            await self.user_datastore.collection.insert_one(await user.to_dict())
+            return web.Response(body="User Created", status=201)
+        except Exception as e:
+            error = {"error": "Failed to create user", "message": str(e)}
+            logger.info(f"{e}")
+            return web.Response(body=json.dumps(error), status=400)
 
     async def get_user(self, request):
-        return web.Response(body="hello", status=200)
+        url = request.rel_url.query
+        try:
+            user_id = extract_single_parameter(url, "user_id", None)
+            user = await self.user_datastore.get_by_attribute(
+                attribute_name="user_id", attribute_value=user_id
+            )
+            user_data = await user[0].to_dict()
+            return web.Response(body=json.dumps(user_data), status=200)
+        except Exception as e:
+            error = {"error": "Failed to get user", "message": str(e)}
+            logger.info(f"{e}")
+            return web.Response(body=json.dumps(error), status=400)
 
     async def post_outfits(self, request):
-        outfit_request = await request.json()
-        self.outfits_datastore.add(User(**outfit_request))
-        return web.Response(body="hello", status=201)
+        try:
+            outfit_request = await request.json()
+            outfit = Outfits(**outfit_request)
+            await self.outfits_datastore.collection.insert_one(await outfit.to_dict())
+            return web.Response(body="Outfit Created", status=201)
+        except Exception as e:
+            error = {"error": "Failed to create outfit", "message": str(e)}
+            logger.info(f"{e}")
+            return web.Response(body=json.dumps(error), status=400)
 
     async def get_outfits(self, request):
-        outfit_id = await request.json()
-        outfits = self.outfits_datastore.get_by_id(outfit_id)
-        return web.Response(body=json.dumps(outfits), status=200)
+        try:
+            outfits = await self.outfits_datastore.get_all()
+            outfits_data = [await outfit.to_dict() for outfit in outfits]
+            return web.Response(body=json.dumps(outfits_data), status=200)
+        except Exception as e:
+            error = {"error": "Failed to get outfits", "message": str(e)}
+            logger.info(f"{e}")
+            return web.Response(body=json.dumps(error), status=400)
+
 
     async def search(self, request):
         url = request.rel_url.query
@@ -62,10 +91,10 @@ class OutfitCreatorService(AbstractApplicationService):
             [
                 web.get(self.base_path + "/test", self.test_endpoint),
                 web.get(self.base_path + "/search", self.search),
-                web.post(self.base_path + "/user", self.test_endpoint),
-                web.get(self.base_path + "/user", self.test_endpoint),
-                web.post(self.base_path + "/outfits", self.test_endpoint),
-                web.get(self.base_path + "/outfits", self.test_endpoint),
+                web.post(self.base_path + "/user", self.post_user),
+                web.get(self.base_path + "/user", self.get_user),
+                web.post(self.base_path + "/outfits", self.post_outfits),
+                web.get(self.base_path + "/outfits", self.get_outfits),
             ]
         )
 
