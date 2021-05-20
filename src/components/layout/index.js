4db5dfd0b9928df7ad04293e11900a1a457829cc
Emma Patricios <emma.patricios@gmail.com>
2021-05-20 16:29:18 +0100

    Adding background and moving share script to index.html

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -139,8 +139,6 @@ class Layout extends Component {
                         </div>
                     </div>
                     <div className="outfit-actions">
-                        <script type="text/javascript"
-                                src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-60a662d10c213301"></script>
                         <ul>
                             <li className="action action--share">
                                 <button className="button button--icon"><span
